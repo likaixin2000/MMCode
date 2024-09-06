08f8fc82681c059f209f137235bf004a3faba7ee
Kaixin Li <likaixin@u.nus.edu>
2024-09-07 00:00:01 +0800

    feat: fine-grained evaluation

--- eval.py
+++ eval.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('.')
-
 import argparse
 from collections import defaultdict
 from testing_utils import run_test
@@ -9,24 +6,22 @@ import multiprocessing
 import numpy as np
 from typing import Dict
 
+import sys
+sys.path.append('.')
+
 from tqdm import tqdm
 
 from utils import load_problems_from_folder, load_problems_from_jsonl
 
-IMAGE_CATEGORIES = [
-    "Linear Data Structure",
-    "Tree",
-    "Graph",
-    "2D Geometry",
-    "3D Geometry",
-    "Chessboard",
-    "Map",
-    "Patterns",
-    "Math",
-    "Table",
-    "Pseudocode",
-    "Others",
-]
+TIMEOUT = 20
+
+import logging
+logging.basicConfig(
+    filename='results/analysis/eval.log',  # Log file path
+    filemode='w',        # 'w' to overwrite the log file each run, 'a' to append
+    level=logging.INFO,  # Minimum log level
+    format='%(levelname)s - %(message)s'  # Log message format
+)
 
 
 def check_correctness(sample, generation, timeout, debug=True):
@@ -69,13 +64,14 @@ def evaluate_generations(generations, samples, idx=None, debug=False):
     results = {}
     idx = 0
     for task_id, problem_generations in tqdm(generations.items()):
+        logging.warning(f"<<< Task {task_id}")
         sample = samples[task_id]
         res = []
         # loop over the generations
         for o_idx, o in enumerate(problem_generations):
             curr_res = [-2]
             try:
-                curr_res = check_correctness(sample, o, timeout=20, debug=debug)
+                curr_res = check_correctness(sample, o, timeout=TIMEOUT, debug=debug)
                 print(curr_res)
                 if debug:
                     print(f"\nSuccessful compilation of task {o_idx}!")
@@ -99,6 +95,7 @@ def evaluate_generations(generations, samples, idx=None, debug=False):
                 res.append(curr_res)
         results[task_id] = res
         idx += 1
+        logging.warning(f" Task {task_id} checked >>>")
     return results
 
 
@@ -120,37 +117,62 @@ def estimate_pass_at_k(num_samples, num_correct, k):
     return np.array([estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)])
 
 
-def compute_metrics(results, problems, k_list=[1, 10, 100], details=True):
+
+def compute_metrics_single(results, k_list):
+    # Compute metrics for a single task
     total = []
     correct = []
 
     correct_by_task = defaultdict(list)
     total_by_task = defaultdict(list)
-
+    
     task_ids = []
     for task_id, res in results.items():
         all_correct = []
-        all_correct_test_case = []
         for generation in res:
-            verdict = np.array(generation)
-            all_correct.append(np.all(verdict>0))
+            verdicts = np.array(generation)
+            all_correct.append(np.all(verdicts>0))
         task_ids.append(task_id)
+        # Append #correct solutions of each task
         correct.append(sum(all_correct))
+        # Append #total solutions of each task
         total.append(len(all_correct))
 
         correct_by_task[task_id] = int(sum(all_correct))
         total_by_task[task_id] = int(len(all_correct))
+    # By task
     total = np.array(total)
     correct = np.array(correct)
     ks = k_list
     detail_pass_at_k = {f"pass@{k}": estimate_pass_at_k(total, correct, k).tolist() for k in ks if (total >= k).all()}
     result_dict = {f"pass@{k}": estimate_pass_at_k(total, correct, k).mean() for k in ks if (total >= k).all()}
-    if details:
-        detail_metrics = {k:dict(zip(task_ids, v)) for k, v in detail_pass_at_k.items()}
-        result_dict["detail"] = detail_metrics
-
+    detail_metrics = {k:dict(zip(task_ids, v)) for k, v in detail_pass_at_k.items()}
+    result_dict["detail"] = detail_metrics
     return result_dict
 
+def compute_metrics(results, problems, k_list=[1, 5, 10]):
+    # Compute metrics for all tasks
+    all_result_dict = compute_metrics_single(results, k_list=k_list)
+    
+    # Find metrics for each image category
+    tasks_by_image_category = defaultdict(dict)
+    for task_id in results.keys():
+        problem = problems[task_id]
+        image_tags = set(problem["image_tags"])
+        for image_tag in image_tags:
+            tasks_by_image_category[image_tag][task_id] = results[task_id]
+            
+    categories_result_dict = {}
+    for image_tag in tasks_by_image_category.keys():
+        task_ids = tasks_by_image_category[image_tag]
+        result_dict = compute_metrics_single({task_id: results[task_id] for task_id in task_ids}, k_list=k_list)
+        all_result_dict[image_tag] = result_dict
+
+    final_result_dict = {
+        'all': all_result_dict,
+        'categories': categories_result_dict
+    }
+    return final_result_dict
 
 def parse_args():
     # Create the parser
@@ -168,13 +190,14 @@ def parse_args():
                         type=str, 
                         required=True, 
                         help='File containing generations to be evaluated.')
+    parser.add_argument('-k', type=str, default='1,5,10', help='The value of k for pass@k metric.')
 
     # Parse and return the arguments
     args = parser.parse_args()
     args.data_split = args.data_split.split(',')
     args.image_categories = args.image_categories.split(',') if args.image_categories else None
+    args.k = [int(k) for k in args.k.split(',')]
     return args
-
 
 def main(args):
     # Load parameters
@@ -191,19 +214,9 @@ def main(args):
     generations = load_generation(generation_file)
 
     results = evaluate_generations(generations, problems, debug=False)
-    # Overall metrics
-    metrics = {}
-    metrics["overall"] = compute_metrics(results, problems)
-    metrics["categories"] = {}
-    # Metrics per image category
-    for image_category in IMAGE_CATEGORIES:
-        image_category_problems = {k: v for k, v in problems.items() if image_category in v["image_tags"]}
-        image_category_results = {k: v for k, v in results.items() if k in image_category_problems}
-        image_category_metrics = compute_metrics(image_category_results, image_category_problems, details=False)
-        metrics["categories"][image_category] = image_category_metrics
+    metrics = compute_metrics(results, problems, k_list=args.k)
 
     json.dump(metrics, open(os.path.join(os.path.dirname(args.generation_file), f'results_{gen_file_basename}.json'), 'w'), indent=4)
-
 
 if __name__ == "__main__":
     args = parse_args()
